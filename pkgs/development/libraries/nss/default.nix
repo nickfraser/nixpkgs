b8e9f79b89c6ab5fff7ed24995d1fdfe149ed66f
Nick Fraser <icanlosh@gmail.com>
2025-10-21 21:39:56 +0100

    [nss] Source patch from archive.org

--- pkgs/development/libraries/nss/default.nix
+++ pkgs/development/libraries/nss/default.nix
@@ -2,7 +2,7 @@
 
 let
   nssPEM = fetchurl {
-    url = http://dev.gentoo.org/~polynomial-c/mozilla/nss-3.15.4-pem-support-20140109.patch.xz;
+    url = https://web.archive.org/web/20210305023555if_/https://dev.gentoo.org/~polynomial-c/mozilla/nss-3.15.4-pem-support-20140109.patch.xz;
     sha256 = "10ibz6y0hknac15zr6dw4gv9nb5r5z9ym6gq18j3xqx7v7n3vpdw";
   };
   version = "3.49.2";
